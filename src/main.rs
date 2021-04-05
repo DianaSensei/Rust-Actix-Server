4c13390a849f07c7e283167718d6cbffb64fca1b
thong.nguyen <thong.nguyen@mservice.com.vn>
2021-04-05 13:47:59 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -16,34 +16,27 @@ mod utils;
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    use crate::nats_broker::*;
-    
-    use crate::core::redis_db::*;
-    use crate::core::nats_server;
-    
+    use crate::nats_broker::*;    
     use actix_web::middleware::errhandlers::{ErrorHandlerResponse, ErrorHandlers};
 
     dotenv::dotenv().ok();
     std::env::set_var("RUST_LOG", "actix_web=info,actix_server=info");
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
-    let redis_fac = RedisFactory::connect(config::CONFIG.redis_url.to_owned())
-        .await
-        .expect("Connect Redis Fail");
-    // let rabbit_fac = RabbitFactory::get_pool(config::CONFIG.rabbit_url.to_owned())
+    // let redis_fac = RedisFactory::connect(config::CONFIG.redis_url.to_owned())
     //     .await
-    //     .expect("Connect Rabbit Fail");
-    let nats_fac = NatsFactory::get_pool(config::CONFIG.nats_url.to_owned())
-        .await
-        .expect("Connect Nats Fail");
+    //     .expect("Connect Redis Fail");
+    // let nats_fac = NatsFactory::get_pool(config::CONFIG.nats_url.to_owned())
+    //     .await
+    //     .expect("Connect Nats Fail");
 
     // rabbit_server::rabbit_server(rabbit_fac.clone()).await;
-    nats_server::nats_server(nats_fac.clone()).await; //Start Nats server
+    // nats_server::nats_server(nats_fac.clone()).await; //Start Nats server
     let mut server = actix_web::HttpServer::new(move || {
         actix_web::App::new()
-            .data(redis_fac.clone()) //Use Redis
+            // .data(redis_fac.clone()) //Use Redis
             // .data(rabbit_fac.clone()) //Use Rabbit
-            .data(nats_fac.clone()) //Use Nats
+            // .data(nats_fac.clone()) //Use Nats
             .wrap(actix_web::middleware::Logger::default())
             .data(
                 actix_web::web::JsonConfig::default()
@@ -68,14 +61,14 @@ async fn main() -> std::io::Result<()> {
                     Ok(ErrorHandlerResponse::Response(res))
                 },
             ))
-            .configure(app::routes::init_route)
+            // .configure(app::routes::init_route)
             .default_service(actix_web::web::route().to(|| actix_web::HttpResponse::NotFound()))
     });
-    let mut listenfd = listenfd::ListenFd::from_env();
-    server = if let Some(l) = listenfd.take_tcp_listener(0)? {
-        server.listen(l)?
-    } else {
-        server.bind(&config::CONFIG.server)?
-    };
-    server.run().await
+    // let mut listenfd = listenfd::ListenFd::from_env();
+    // server = if let Some(l) = listenfd.take_tcp_listener(0)? {
+    //     server.listen(l)?
+    // } else {
+    //     server.bind(&config::CONFIG.server)?
+    // };
+    // server.run().await
 }
