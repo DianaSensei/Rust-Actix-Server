1e67646db6e9cc713b854639ca321c08531b16c7
NGUYỄN ĐỨC THÔNG - ITC - M4B - Junior Java Developer <thong.nguyen@mservice.com.vn>
2022-08-10 20:18:00 +0700

    update

--- src/services/clients/postgres_client_service.rs
+++ src/services/clients/postgres_client_service.rs
@@ -1,16 +1,15 @@
 use crate::settings;
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
+use diesel_migrations::EmbeddedMigrations;
 use once_cell::sync::Lazy;
-
-embed_migrations!();
 
 type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
 pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!();
 
 static DB_CONNECTION_POOL: Lazy<Pool> = Lazy::new(|| {
-    let manager =
-        ConnectionManager::<PgConnection>::new(&*settings::SETTINGS.datasource.database.url);
+    let manager = ConnectionManager::<PgConnection>::new(&*settings::SETTINGS.datasource.database.url);
     let pool = Pool::builder()
         .build(manager)
         .expect("Failed to create database connection pool");
@@ -18,12 +17,14 @@ static DB_CONNECTION_POOL: Lazy<Pool> = Lazy::new(|| {
     pool
 });
 
-pub fn init_and_run_migration() {
-    let conn = get_database_connection();
-    // run_pending_migrations(&conn);
-    embedded_migrations::run_with_output(&conn, &mut std::io::stdout())
-        .expect("Run Postgres Migration Fail");
-}
+// pub fn init_and_run_migration() {
+//     let conn = get_database_connection();
+//     // run_pending_migrations(&conn);
+//     //
+//     // (MIGRATIONS).unwrap();
+//     embedded_migrations::run_with_output(&conn, &mut std::io::stdout())
+//         .expect("Run Postgres Migration Fail");
+// }
 
 pub fn get_database_connection() -> DbConnection {
     DB_CONNECTION_POOL
